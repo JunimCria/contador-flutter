5734f09a95027e948a46fbd4fbb7fd4b9cf9235f
JunimCria <gilbertojuniorcc@gmail.com>
2023-01-25 23:29:32 -0300

    Finalizando Projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,148 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     // Fazendo override em método herdado do Stateless Widget
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  void decrement(){
-    print('decrement');
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  bool _zeroperson = true;
+  bool _fullperson = false;
+  int person = 0;
+
+  void decrement() {
+    setState(() {
+      if (!_zeroperson) {
+        person--;
+      }
+    });
   }
-  void increment(){
-    print('increment');
+
+  void increment() {
+    setState(() {
+      if (!_fullperson) {
+        person++;
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
+    if (person == 0) {
+      _zeroperson = true;
+    } else {
+      _zeroperson = false;
+    }
+    if (person >= 20) {
+      _fullperson = true;
+    } else {
+      _fullperson = false;
+    }
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Pode entrar!',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/images/image2.jpg'),
           ),
-          const Text(
-            '11',
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _fullperson ? 'Lotaado!!' : 'Pode entrar!',
+              style: TextStyle(
+                fontSize: 35,
+                color: _fullperson ? Colors.red : Colors.black,
+                fontWeight: FontWeight.w900,
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:  [
-              TextButton(
-                onPressed: decrement,
-                child: const Text(
-                  'Saiu',
-                  style: TextStyle(
-                    color: Colors.white,
+            const SizedBox(
+              height: 70,
+            ),
+            Text(
+              '$person',
+              style: const TextStyle(
+                fontSize: 100,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: _zeroperson ? Colors.red : Colors.white,
+                    fixedSize: const Size(120, 120),
+                    foregroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(60),
+                      side: BorderSide(
+                        width: 3,
+                      ),
+                    ),
+                  ),
+                  onPressed: _zeroperson ? null : decrement,
+                  child: Text(
+                    _zeroperson ? 'Empty' : 'Saiu',
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                    ),
                   ),
                 ),
-              ),
-              TextButton(
-                onPressed: increment,
-                child: const Text(
-                  'Entrou',
-                  style: TextStyle(
-                    color: Colors.white,
+                SizedBox(
+                  width: 50,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: _fullperson ? Colors.red : Colors.white,
+                    fixedSize: const Size(120, 120),
+                    foregroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(60),
+                      side: BorderSide(
+                        width: 3,
+                      ),
+                    ),
+                  ),
+                  onPressed: _fullperson ? null : increment,
+                  child: Text(
+                    _fullperson ? 'Full' : 'Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
